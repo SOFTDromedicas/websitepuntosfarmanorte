8cf5ede54678c419263676c9689cf6c2c97b9f9d
lfernandortiz <lfernandortiz@gmail.com>
2017-08-19 12:46:07 -0500

    :100:% Menu - activacion de menu por navegacion

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).foundation()
 
+var currentElement = null;
 
 //activa menu
  $('.menu li a').click(function(){
@@ -11,3 +12,57 @@ $(document).foundation()
 
 
 
+//activa menu off-canvas
+ $('.vertical .menu li a').click(function(){
+    $('li a').removeClass("activa");
+    $(this).addClass("activa");                                            
+});		
+//Activa opcion del menu cuando la navegacion es por scroll
+var lastId,
+    topMenu = $("#menppal"),
+
+    topMenuHeight = topMenu.outerHeight()+15;
+    // All list items
+    menuItems = topMenu.find("a");    
+
+			console.log(topMenu);
+
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e){
+	  var href = $(this).attr("href"),
+	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+	  $('html, body').stop().animate({ scrollTop: offsetTop }, 300);
+	  e.preventDefault();
+	});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position  
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+    
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+
+       return this;
+   });
+   // Get the id of the current element     	
+   // Get id of current scroll item
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;       
+       if(currentElement != null){       
+       	 if(currentElement.attr("href").replace("#","") !== id);       		
+       	 	currentElement.removeClass("activa");
+       }
+       // Set/remove active class
+       menuItems.parent().removeClass("activa").end().filter("[href='#"+id+"']").parent().addClass("activa");
+   }                   
+});
