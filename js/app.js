78913ea1dc3d4846acf80b941bbe55202b602ab6
lfernandortiz <lfernandortiz@gmail.com>
2017-08-19 10:41:28 -0500

    Activacion menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 $(document).foundation()
 
 
+//activa menu
+ $('.menu li a').click(function(){
+    $('li').removeClass("activa");
+    // $(this).addClass("activation");
+    currentElement = $(this);
+    currentElement.parent().addClass("activa");
+});
+
+
 
