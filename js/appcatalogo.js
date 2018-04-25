f45f3b6771e4dd4f65c1ff73856efb34a205731d
lfernandortiz <lfernandortiz@gmail.com>
2018-04-25 11:49:48 -0500

    :push: Prototipo de la grilla del catalogo para el DOM

--- js/appcatalogo.js
+++ js/appcatalogo.js
@@ -10,25 +10,60 @@ var catalogoProductos;
 
 
 function init() {
+    //llamada al End-Point de Dropos que expone el catalogo de puntos.
+    //obtenerDatosCatalogo();
 
-    obtenerDatosCatalogo();
+    testCatalogo();
 
 }
 
+//prototipo para generar la grilla DOM  del catalogO
+function testCatalogo(){
+    var catalogo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+   
+    var lengthC = Math.round(catalogo.length / 4);
+    var multiArray = [];
+    
+    for( var i = 0 ; i < lengthC; i++ ){
+        var x = [];
+        multiArray.push(x);
+    }
+    
+    var flag = 0;
+
+    for( var i= 0 ; i <= catalogo.length+1; i++){
+        if( flag < multiArray.length ){
+            if( catalogo[i] != null ){
+                multiArray[flag].push( catalogo[i]); 
+            }
+            if( multiArray[flag].length == 4){
+                flag++;
+            }
+        }
+    }
+
+    console.log(multiArray);
+}
+
+
 function obtenerDatosCatalogo() {
-    obtenerInfoAfiliado(function (response) {
+    obtenerCatalogoServicio(function (response) {
         //Parse JSON string into object
-        infoAfiliado = JSON.parse(response);
-        if (infoAfiliado.code == 200) {
-            console.log(infoAfiliado);
-            //cargarDatosCatalogo();
+        var dataTemp = JSON.parse(response);
+        //inicia el los objetos del catalogo
+        catalogoProductos = dataTemp.data;
+        if (dataTemp.code == 200) {
+            //llena el DOM con el catalogo
+           cargarDatosCatalogo();
+           console.log(catalogoProductos.length);
+           console.log(catalogoProductos);
         } else {
             //aca manejo el error si no hay funciona el servicio
         }
     });
 }
 
-function obtenerInfoAfiliado(callback) {
+function obtenerCatalogoServicio(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open('GET', urlServicioCatalogo, true); // Replace 'my_data' with the path to your file
@@ -41,6 +76,26 @@ function obtenerInfoAfiliado(callback) {
 }
 
 function cargarDatosCatalogo() {
+
+    //elemento del DOM que alberga el catalogo de prodcutos
+    var contenedorCatalogo = document.querySelector('#contenedor-catalogo');
+
+    if(catalogoProductos.length > 0){
+
+        for (var index = 0; index < contenedorCatalogo.length; index++) {
+            //evalua si es el comienzo del catalogo
+            if( index == 0 ){
+                var rowContenedor = document.createElement("div");
+                rowContenedor.setAttribute("class", "row small-up-2 large-up-4 row-productos")
+            }
+
+
+
+        }
+
+    }else{
+
+    }
     
 
 }
