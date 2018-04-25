ce06edd9759d0d6da090d59d660b9ea4c150061d
lfernandortiz <lfernandortiz@gmail.com>
2018-04-25 10:17:31 -0500

    Cambios menores de js

--- js/appcatalogo.js
+++ js/appcatalogo.js
@@ -1,11 +1,61 @@
 //@utor-> @lfernandortiz | @SOFTDromedicas
 $(document).foundation()
+//registro de manejo de eventos para la carga de la pagina
+window.addEventListener("load", init, false);
+
+var urlServicioCatalogo = "http://localhost/dropos/wsjson/catalogopuntos/?opc=catalogo";
+var urlImgCatalogo = "http://localhost/dropos/imagenes.php?opcion=getimgcatalogo&productoid=";
+var urlImgDetalle = "http://localhost/dropos/imagenes.php?opcion=getimgdetalle&productoid=";
+var catalogoProductos;
+
+
+function init() {
+
+    obtenerDatosCatalogo();
+
+}
+
+function obtenerDatosCatalogo() {
+    obtenerInfoAfiliado(function (response) {
+        //Parse JSON string into object
+        infoAfiliado = JSON.parse(response);
+        if (infoAfiliado.code == 200) {
+            console.log(infoAfiliado);
+            //cargarDatosCatalogo();
+        } else {
+            //aca manejo el error si no hay funciona el servicio
+        }
+    });
+}
+
+function obtenerInfoAfiliado(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', urlServicioCatalogo, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function cargarDatosCatalogo() {
+    
+
+}
+
+
+
+
+
+
 
 
 var currentElement = null;
 
 //activa menu
- $('.menu li a').click(function(){
+$('.menu li a').click(function () {
     $('li').removeClass("activa");
     // $(this).addClass("activation");
     currentElement = $(this);
@@ -14,12 +64,12 @@ var currentElement = null;
 
 
 //close off-canvas
-$('.off-canvas a').on('click', function() {
-  $('.off-canvas').foundation('close');
+$('.off-canvas a').on('click', function () {
+    $('.off-canvas').foundation('close');
 });
 
- //activa menu
- $('#menuoffcanvas li a').click(function(){
+//activa menu
+$('#menuoffcanvas li a').click(function () {
     $('li').removeClass("activaOffCanvas");
     // $(this).addClass("activation");
     currentElement = $(this);
@@ -27,8 +77,8 @@ $('.off-canvas a').on('click', function() {
 });
 
 //activa menu off-canvas
- $('.vertical .menu li a').click(function(){
+$('.vertical .menu li a').click(function () {
     $('li a').removeClass("activa");
-    $(this).addClass("activa");                                            
-});   
+    $(this).addClass("activa");
+});
 
